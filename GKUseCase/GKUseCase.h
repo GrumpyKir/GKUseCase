f5b6b4976cb4119434946b3e2706941269a7eaa0
Kirill Opekishev <grumpykir@gmail.com>
2019-04-12 14:24:51 +0300

    Initial Commit

---
+++ GKUseCase/GKUseCase.h
@@ -0,0 +1,19 @@
+//
+//  GKUseCase.h
+//  GKUseCase
+//
+//  Created by  Кирилл on 4/12/19.
+//  Copyright © 2019 AppCraft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GKUseCase.
+FOUNDATION_EXPORT double GKUseCaseVersionNumber;
+
+//! Project version string for GKUseCase.
+FOUNDATION_EXPORT const unsigned char GKUseCaseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GKUseCase/PublicHeader.h>
+
+
